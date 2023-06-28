9069f9479574df5064fbbd3068bb5f20d7361ac6
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2023-06-28 15:39:35 +0200

    feat: add cards style, add images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,27 @@ export class AppComponent {
     { title: 'Mountain Biking',
       imageUrl: 'assets/biking.jpeg',
       username: 'biking12222',
-      content: 'Action shot of me riding my bike '
+      content: 'Action shot of me riding my bike'
+    },
+    { title: 'Ocean waves',
+      imageUrl: 'assets/ocean.jpg',
+      username: 'journeyS',
+      content: 'Great ocean waves'
+    },
+    { title: 'Clouds with sunlight',
+      imageUrl: 'assets/clouds.JPG',
+      username: 'flyinglover123',
+      content: 'My favorite time of traveling'
+    },
+    { title: 'Boats on the lake',
+      imageUrl: 'assets/boats.jpg',
+      username: 'hobbyever',
+      content: 'Weekend plans'
+    },
+    { title: 'Retro car',
+      imageUrl: 'assets/car.jpg',
+      username: 'autoY__Y',
+      content: 'Lovely retro'
     }
   ]
 }
