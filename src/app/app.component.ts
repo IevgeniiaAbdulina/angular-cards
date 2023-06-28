2f1aa129b82216f3df0d4f31df93f29e5c9aa6a7
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2023-06-28 12:56:55 +0200

    feat: accepting  Data in a child Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    { title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw thie neat tree today'
+    },
+    { title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Beautiful view of some mountains I saw during my hike'
+    },
+    { title: 'Mountain Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking12222',
+      content: 'Action shot of me riding my bike '
+    }
+  ]
 }
